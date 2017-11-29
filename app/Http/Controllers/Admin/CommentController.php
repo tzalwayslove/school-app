1f1a2e97833e7a0e69a6afba7ebfc2221505023e
songzidaiyu <1151879477@qq.com>
2017-11-29 15:40:28 +0800

    1

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -16,7 +16,6 @@ class CommentController extends Controller
      */
     protected $validateRoule = [
         'content' => 'required|max:100',
-        'zan' => 'required|max:100',
         'articel' => 'required|exists:articel,id',
         'creatd_at' => 'required|max:100',
     ];
