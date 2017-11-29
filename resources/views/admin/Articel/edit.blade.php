6c5acbc0817b08956ecabb184462a71df3b915cb
songzidaiyu <1151879477@qq.com>
2017-11-29 14:51:46 +0800

    1

--- resources/views/admin/Articel/edit.blade.php
+++ resources/views/admin/Articel/edit.blade.php
@@ -14,81 +14,82 @@
                     <div class="x_content">
                         <form class="form-horizontal form-label-left" novalidate data-parsley-validate
                               action="{{  url("admin/articel/$data->id") }}" method="post">
-                        {{ csrf_field() }}
-                        {{ method_field('PATCH') }}
-                        <span class="section">Articel信息</span>
-                                                                                                                                            <div class="item form-group">
+                            {{ csrf_field() }}
+                            {{ method_field('PATCH') }}
+                            <span class="section">Articel信息</span>
+                            <div class="item form-group">
                                 <label class="control-label col-md-3 col-sm-3 col-xs-12"
                                        for="content">内容</label>
                                 <div class="col-md-6 col-sm-6 col-xs-12">
-                                    <textarea id="content" name="content" class="form-control col-md-7 col-xs-12">{{ $data->content }}</textarea>
+                                    <textarea id="content" name="content"
+                                              class="form-control col-md-7 col-xs-12">{{ $data->content }}</textarea>
                                 </div>
                             </div>
-                                                                                                                                            <div class="item form-group">
+                            <div class="item form-group">
                                 <label class="control-label col-md-3 col-sm-3 col-xs-12"
                                        for="user">用户名</label>
                                 <div class="col-md-6 col-sm-6 col-xs-12">
                                     <select name="user" id="user" class="form-control">
-                                            @foreach($user_accounts as $user)
-                                                <option
-                                                 @if($user->id == $data->user)
+                                        @foreach($user_accounts as $user)
+                                            <option
+                                                    @if($user->id == $data->user)
                                                     selected="true"
-                                                @endif
-                                                 value="{{ $user->id }}">{{ $user->account }}</option>
-                                            @endforeach
-                                        </select>
+                                                    @endif
+                                                    value="{{ $user->id }}">{{ $user->account }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
                             </div>
-                                                                                <div class="item form-group">
+                            <div class="item form-group">
                                 <label class="control-label col-md-3 col-sm-3 col-xs-12"
                                        for="cate">分类</label>
                                 <div class="col-md-6 col-sm-6 col-xs-12">
                                     <select name="cate" id="cate" class="form-control">
-                                            @foreach($cate_names as $cate)
-                                                <option
-                                                 @if($cate->id == $data->cate)
+                                        @foreach($cate_names as $cate)
+                                            <option
+                                                    @if($cate->id == $data->cate)
                                                     selected="true"
-                                                @endif
-                                                 value="{{ $cate->id }}">{{ $cate->name }}</option>
-                                            @endforeach
-                                        </select>
+                                                    @endif
+                                                    value="{{ $cate->id }}">{{ $cate->name }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
                             </div>
-                                                                                <div class="item form-group">
+                            <div class="item form-group">
                                 <label class="control-label col-md-3 col-sm-3 col-xs-12"
                                        for="show">是否显示</label>
                                 <div class="col-md-6 col-sm-6 col-xs-12">
-                                    <input type='hidden' name='show' value='0'/>
-<input type="checkbox"
-                                                 @if($data->show)
-                                                    checked="true"
-                                                @endif
-                                                 class="js-switch" data-switchery="true" name="show" id="show" value="{{ $data->show }}">
+                                    <input type="checkbox"
+                                           @if($data->show)
+                                           checked="true"
+                                           @endif
+                                           class="js-switch" data-switchery="true" name="show" id="show"
+                                           value="{{ $data->show }}">
                                 </div>
                             </div>
-                                                                                                                                                                                                            
 
-                        <div class="ln_solid"></div>
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-3">
-                                <button type="reset" class="btn btn-primary">取消</button>
-                                <button id="send" type="submit" class="btn btn-success">确定</button>
+
+                            <div class="ln_solid"></div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-3">
+                                    <button type="reset" class="btn btn-primary">取消</button>
+                                    <button id="send" type="submit" class="btn btn-success">确定</button>
+                                </div>
                             </div>
-                        </div>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endsection
+@endsection
 
 @push('addcss')
-    <link href="{{ asset('public/vendors/switchery/dist/switchery.min.css') }}" rel="stylesheet">
+<link href="{{ asset('public/vendors/switchery/dist/switchery.min.css') }}" rel="stylesheet">
 @endpush
 @push('addjs')
 <script src="{{ asset('public/vendors/validator/validator.js') }}"></script>
-    <script src="{{ asset('public/vendors/switchery/dist/switchery.min.js') }}"></script>
+<script src="{{ asset('public/vendors/switchery/dist/switchery.min.js') }}"></script>
 <script>
     @foreach($errors->all() as $e)
         new PNotify({
@@ -98,6 +99,6 @@
     });
     @endforeach
 
-    
+
 </script>
 @endpush
