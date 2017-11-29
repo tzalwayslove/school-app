d6a1f2451d4268c145022e9d7c1f8a95a2d2ce87
songzidaiyu <1151879477@qq.com>
2017-11-29 16:30:59 +0800

    1

--- resources/views/admin/articel/edit.blade.php
+++ resources/views/admin/articel/edit.blade.php
@@ -149,7 +149,7 @@
             methods: {
                test: function (item, event) {
                     id = item.id;
-                    console.log($(event.target).checked);
+                    console.log($(event.target).attr('checked'));
                     return;
                     $.post($('#comments').data('show_url'), {
                         id: id,
