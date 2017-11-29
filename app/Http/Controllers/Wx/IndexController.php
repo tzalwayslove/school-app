6134c7dc00cb0f682df5f29c026ebc0f3497ba01
songzidaiyu <1151879477@qq.com>
2017-11-29 10:55:50 +0800

    1

--- app/Http/Controllers/Wx/IndexController.php
+++ app/Http/Controllers/Wx/IndexController.php
@@ -11,44 +11,51 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $option = require 'wechatConfig.php';
+        try {
 
-        $app = new Application($option);
 
-        $server = $app->server;
+            $option = require 'wechatConfig.php';
 
-        $server->setMessageHandler(function ($message) {
-            Log::log($message->MsgType);
-            switch ($message->MsgType) {
-                case 'event':
-                    return '收到事件消息';
-                    break;
-                case 'text':
-                    return '收到文字消息';
-                    break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                // ... 其它消息
-                default:
-                    return '收到其它消息';
-                    break;
-            }
-        });
+            $app = new Application($option);
 
-        $response = $server->serve();
-        return $response;
+            $server = $app->server;
+
+            $server->setMessageHandler(function ($message) {
+                Log::log($message->MsgType);
+                switch ($message->MsgType) {
+                    case 'event':
+                        return '收到事件消息';
+                        break;
+                    case 'text':
+                        return '收到文字消息';
+                        break;
+                    case 'image':
+                        return '收到图片消息';
+                        break;
+                    case 'voice':
+                        return '收到语音消息';
+                        break;
+                    case 'video':
+                        return '收到视频消息';
+                        break;
+                    case 'location':
+                        return '收到坐标消息';
+                        break;
+                    case 'link':
+                        return '收到链接消息';
+                        break;
+                    // ... 其它消息
+                    default:
+                        return '收到其它消息';
+                        break;
+                }
+            });
+
+            $response = $server->serve();
+            return $response;
+        } catch (\Exception $e) {
+            Log::log($e->getMessage() . $e->getFile() . $e->getLine());
+        }
+        return '';
     }
 }
