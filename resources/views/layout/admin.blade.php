16ecc876363705b7cfe0dc7903ce1c405c1aec74
songzidaiyu <1151879477@qq.com>
2017-11-29 10:33:51 +0800

    1

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -61,13 +61,7 @@
                     <div class="menu_section">
                         <h3>General</h3>
                         <ul class="nav side-menu">
-                            <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="index.html">Dashboard</a></li>
-                                    <li><a href="index2.html">Dashboard2</a></li>
-                                    <li><a href="index3.html">Dashboard3</a></li>
-                                </ul>
-                            </li>
+
                             <li><a><i class="fa fa-edit"></i> 学生列表 <span class="fa fa-chevron-down"></span></a>
                                 <ul class="nav child_menu">
                                     <li><a href="{{ url('admin/posts') }}">所有学生</a></li>
