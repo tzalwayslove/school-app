ad0d7177c97eaff68ac2c690ba64c3c61e825b31
songzidaiyu <1151879477@qq.com>
2017-11-29 11:06:01 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::post('code', 'Admin\CodeController@gettable');
 Route::put('code', 'Admin\CodeController@settable');
 
 Route::any('/wx', 'Wx\IndexController@index');
+Route::any('/wx',function (){
+    \App\Model\Log::log(1111111111111111);
+});
