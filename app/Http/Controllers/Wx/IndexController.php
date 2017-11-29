87a32b686c3b39ba72547378a8fd292e229f4aba
songzidaiyu <1151879477@qq.com>
2017-11-29 13:01:15 +0800

    1

--- app/Http/Controllers/Wx/IndexController.php
+++ app/Http/Controllers/Wx/IndexController.php
@@ -19,51 +19,52 @@ class IndexController extends Controller
             $app = new Application($option);
 
             $server = $app->server;
-
-            $server->setMessageHandler(function ($message)use($server) {
-                Log::log($message->MsgType);
-                switch ($message->MsgType) {
-                    case 'event':
-                        return '收到事件消息';
-                        break;
-                    case 'text':
-                        switch ($message->Content){
-                            case "绑定":
-                                User::createInit($message->FromuserName);
-                                return '请输入您的学号:';
-                            default:
-                                try{
-                                    $res = User::bind($message->FromuserName, $message);
-                                    return $res;
-                                }catch(userNotFountException $e){
-                                    return '输入绑定即可进入绑定流程';
-                                }
-                        }
+            try {
+                $server->setMessageHandler(function ($message) use ($server) {
+                    switch ($message->MsgType) {
+                        case 'event':
+                            return '收到事件消息';
+                            break;
+                        case 'text':
+                            switch ($message->Content) {
+                                case "绑定":
+                                    User::createInit($message->FromuserName);
+                                    return '请输入您的学号:';
+                                default:
+                                    try {
+                                        $res = User::bind($message->FromuserName, $message);
+                                        return $res;
+                                    } catch (userNotFountException $e) {
+                                        return '输入绑定即可进入绑定流程';
+                                    }
+                            }
 //                        return json_encode($message, JSON_UNESCAPED_UNICODE);
 
-                        break;
-                    case 'image':
-                        return '收到图片消息';
-                        break;
-                    case 'voice':
-                        return '收到语音消息';
-                        break;
-                    case 'video':
-                        return '收到视频消息';
-                        break;
-                    case 'location':
-                        return '收到坐标消息';
-                        break;
-                    case 'link':
-                        return '收到链接消息';
-                        break;
-                    // ... 其它消息
-                    default:
-                        return '收到其它消息';
-                        break;
-                }
-            });
-
+                            break;
+                        case 'image':
+                            return '收到图片消息';
+                            break;
+                        case 'voice':
+                            return '收到语音消息';
+                            break;
+                        case 'video':
+                            return '收到视频消息';
+                            break;
+                        case 'location':
+                            return '收到坐标消息';
+                            break;
+                        case 'link':
+                            return '收到链接消息';
+                            break;
+                        // ... 其它消息
+                        default:
+                            return '收到其它消息';
+                            break;
+                    }
+                });
+            } catch (\Exception $e) {
+                Log::log($e->getMessage() . $e->getFile() . $e->getLine());
+            }
             $response = $server->serve();
             return $response;
         } catch (\Exception $e) {
