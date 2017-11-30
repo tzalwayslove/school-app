5ee26f94e20f8442b100fe3f81f93ad229895713
songzidaiyu <1151879477@qq.com>
2017-11-30 17:11:34 +0800

    1

--- public/vue/index.js
+++ public/vue/index.js
@@ -59,15 +59,13 @@ tiezi = Vue.component('tiezi', function (success, error) {
                     })
                 },
                 setZan: function (item) {
-                    console.log(item);
-                    return;
                     zan = !item.zanLog ? 1 : -1;
                     id = item.id;
                     axios.post('articel_zan', {
                         id: id,
                         zan: zan
                     }).then(function (res) {
-                        console.log(res);
+                        console.log(res.data.articel);
                         item = res.data.articel
                     });
                 },
