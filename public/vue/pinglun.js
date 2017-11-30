d4ec523e5aa5919499dc0120edf0ab40ff6bea33
songzidaiyu <1151879477@qq.com>
2017-11-30 16:21:31 +0800

    1

--- public/vue/pinglun.js
+++ public/vue/pinglun.js
@@ -18,7 +18,7 @@ pinglun = Vue.component('pinglun', function (success, error) {
                     axios.get('wx/comment/'+ id).then(function(res){
                         $this.articel = res.data.data;
                         $this.list = res.data.data.get_comment;
-                        console.log(res.data);
+                        console.log(res.data.data);
                         if($this.list.get_comment.length == 0){
                             alert('还没有人评论');
                         }
