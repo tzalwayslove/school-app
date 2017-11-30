e34ebfed3ab2a1a15617cf0922101374272e3f43
songzidaiyu <1151879477@qq.com>
2017-11-30 11:52:36 +0800

    1

--- resources/views/admin/wx/index.blade.php
+++ resources/views/admin/wx/index.blade.php
@@ -33,7 +33,6 @@
                                  v-bind:class="['weui-tabbar__item', nav_active == 'fabu' ? 'weui-fabu__item_on' : '']">
                     <span style="display: inline-block;position: relative;">
                         <img src="{{asset('public/images/icon_tabbar.png')}}" alt="" class="weui-tabbar__icon">
-                        <span class="weui-badge" style="position: absolute;top: -2px;right: -13px;">8</span>
                     </span>
                         <p class="weui-tabbar__label">帖子</p>
                     </router-link>
@@ -46,7 +45,6 @@
                                  v-bind:class="['weui-tabbar__item', nav_active == 'wode' ? 'weui-fabu__item_on' : '']">
                     <span style="display: inline-block;position: relative;">
                         <img src="{{asset('public/images/icon_tabbar.png')}}" alt="" class="weui-tabbar__icon">
-                        <span class="weui-badge weui-badge_dot" style="position: absolute;top: 0;right: -6px;"></span>
                     </span>
                         <p class="weui-tabbar__label">发现</p>
                     </router-link>
