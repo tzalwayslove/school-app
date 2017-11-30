cba893d922baa798693b8f573407cd9b7ef45b90
songzidaiyu <1151879477@qq.com>
2017-11-30 12:59:00 +0800

    1

--- resources/views/admin/wx/index.blade.php
+++ resources/views/admin/wx/index.blade.php
@@ -154,7 +154,9 @@
                 console.log($('#scrollPanel')[0].scrollHeight);*/
                 var bottom = $('#scrollPanel')[0].scrollHeight - $('#scrollPanel')[0].scrollTop - $('#scrollPanel')[0].offsetHeight
 
-                console.log(bottom);
+                if(bottom < 300){
+                    console.log('加载');
+                }
             }
         },
     }).$mount('#app');
