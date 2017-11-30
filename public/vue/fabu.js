dfd7a55d6203160a4c1975023172d0008492645b
songzidaiyu <1151879477@qq.com>
2017-11-30 15:54:08 +0800

    1

--- public/vue/fabu.js
+++ public/vue/fabu.js
@@ -5,9 +5,11 @@ fabu = Vue.component('fabu', function (success, error) {
     axios.get("/public/tpl/fatie.html").then(function (res) {
         success({
             template: res.data,
-            data: {
-                title:'',
-                content:''
+            data(){
+                return {
+                    title:'',
+                    content:''
+                }
             },
             methods:{
                 send: function(){
