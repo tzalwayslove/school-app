08f247e13c76d8520117966259455591217a550d
songzidaiyu <1151879477@qq.com>
2017-11-30 09:27:40 +0800

    1

--- resources/views/admin/wx/index.blade.php
+++ resources/views/admin/wx/index.blade.php
@@ -12,34 +12,33 @@
 <body>
 <div id="app">
     <div class="page">
+        <router-view>
+
+        </router-view>
         <div class="page__bd" style="height: 100%;">
             <div class="weui-tab">
                 <div class="weui-tab__panel">
 
                 </div>
                 <div class="weui-tabbar">
-                    <a href="javascript:;" class="weui-tabbar__item weui-bar__item_on">
+                        <router-link to="/foo" class="weui-tabbar__item weui-bar__item_on">
                     <span style="display: inline-block;position: relative;">
                         <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
                         <span class="weui-badge" style="position: absolute;top: -2px;right: -13px;">8</span>
                     </span>
                         <p class="weui-tabbar__label">微信</p>
-                    </a>
-                    <a href="javascript:;" class="weui-tabbar__item">
+                        </router-link>
+                    <router-link to="/foo" class="weui-tabbar__item">
                         <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
                         <p class="weui-tabbar__label">通讯录</p>
-                    </a>
-                    <a href="javascript:;" class="weui-tabbar__item">
+                    </router-link>
+                    <router-link to="/foo" class="weui-tabbar__item">
                     <span style="display: inline-block;position: relative;">
                         <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
                         <span class="weui-badge weui-badge_dot" style="position: absolute;top: 0;right: -6px;"></span>
                     </span>
                         <p class="weui-tabbar__label">发现</p>
-                    </a>
-                    <a href="javascript:;" class="weui-tabbar__item">
-                        <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
-                        <p class="weui-tabbar__label">我</p>
-                    </a>
+                    </router-link>
                 </div>
             </div>
         </div>
@@ -51,6 +50,8 @@
             });
         });
     </script>
+
+    <router-link to="/bar">bar</router-link>
 
 </div>
 <script src="{{ asset('public/vendors/vue/vue.js') }}"></script>
