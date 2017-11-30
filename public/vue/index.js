a00f5eb48220c6e9e36d57b8384f017d7ea7aa31
songzidaiyu <1151879477@qq.com>
2017-11-30 14:04:51 +0800

    1

--- public/vue/index.js
+++ public/vue/index.js
@@ -17,7 +17,7 @@ tiezi = Vue.component('tiezi', function (success, error) {
                 getData: function(){
                     $this = this;
                     axios.get('/wx/articel', {params:{cate:0, page:this.page}}).then(function(res){
-                        $this.tiezi = res.data.list.data;
+                        $this.tiezi = $this.tiezi + res.data.list.data;
                     });
                 },
                 touchStart: function(e){
