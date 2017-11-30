0fe44439e8d6856b214e184c28f8a1f5d56c35e0
songzidaiyu <1151879477@qq.com>
2017-11-30 09:28:04 +0800

    1

--- resources/views/admin/wx/index.blade.php
+++ resources/views/admin/wx/index.blade.php
@@ -21,24 +21,7 @@
 
                 </div>
                 <div class="weui-tabbar">
-                        <router-link to="/foo" class="weui-tabbar__item weui-bar__item_on">
-                    <span style="display: inline-block;position: relative;">
-                        <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
-                        <span class="weui-badge" style="position: absolute;top: -2px;right: -13px;">8</span>
-                    </span>
-                        <p class="weui-tabbar__label">微信</p>
-                        </router-link>
-                    <router-link to="/foo" class="weui-tabbar__item">
-                        <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
-                        <p class="weui-tabbar__label">通讯录</p>
-                    </router-link>
-                    <router-link to="/foo" class="weui-tabbar__item">
-                    <span style="display: inline-block;position: relative;">
-                        <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon">
-                        <span class="weui-badge weui-badge_dot" style="position: absolute;top: 0;right: -6px;"></span>
-                    </span>
-                        <p class="weui-tabbar__label">发现</p>
-                    </router-link>
+
                 </div>
             </div>
         </div>
