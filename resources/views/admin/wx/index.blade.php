fea7841139d536afb7b64dc5c2b919718ac386e7
songzidaiyu <1151879477@qq.com>
2017-11-30 09:51:28 +0800

    1

--- resources/views/admin/wx/index.blade.php
+++ resources/views/admin/wx/index.blade.php
@@ -94,7 +94,6 @@
         '<div class="weui-cell__bd">' +
         '<p>标题文字</p>' +
         ' </div>' +
-        '<div class="weui-cell__ft">说明文字</div>' +
         '</div>' +
         '</div>' +
         '<div class="weui-cell__ft">' +
