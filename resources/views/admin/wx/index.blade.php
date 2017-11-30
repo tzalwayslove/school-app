fee80f0d3904c75c25df92d3e4015c18c84c8393
songzidaiyu <1151879477@qq.com>
2017-11-30 11:57:28 +0800

    1

--- resources/views/admin/wx/index.blade.php
+++ resources/views/admin/wx/index.blade.php
@@ -30,7 +30,7 @@
                 </div>
                 <div class="weui-tabbar">
                     <router-link to="/tiezi"
-                                 v-bind:class="['weui-tabbar__item, weui-bar__item_on', nav_active == 'tiezi' ? 'weui-fabu__item_on' : '']">
+                                 v-bind:class="['weui-tabbar__item', 'weui-bar__item_on', nav_active == 'tiezi' ? 'weui-fabu__item_on' : '']">
                     <span style="display: inline-block;position: relative;">
                         <img src="{{asset('public/images/icon_tabbar.png')}}" alt="" class="weui-tabbar__icon">
                     </span>
