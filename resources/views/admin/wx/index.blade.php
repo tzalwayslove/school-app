240170c4aef7156af26ffc912e79bfebdfae8731
songzidaiyu <1151879477@qq.com>
2017-11-30 12:59:36 +0800

    1

--- resources/views/admin/wx/index.blade.php
+++ resources/views/admin/wx/index.blade.php
@@ -154,7 +154,8 @@
                 console.log($('#scrollPanel')[0].scrollHeight);*/
                 var bottom = $('#scrollPanel')[0].scrollHeight - $('#scrollPanel')[0].scrollTop - $('#scrollPanel')[0].offsetHeight
 
-                if(bottom < 300){
+                console.log();
+                if(bottom < $('#scrollPanel')[0].scrollHeight / 2){
                     console.log('加载');
                 }
             }
