926045e1c7c3800d25bd28da81e7b70cff092033
songzidaiyu <1151879477@qq.com>
2017-11-30 09:25:19 +0800

    1

--- resources/views/admin/wx/index.blade.php
+++ resources/views/admin/wx/index.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('public/vendors/weui/weui.css') }}">
-
+    <script src="{{ asset('public/vendors/weui/zepto.min.js') }}"></script>
     <title></title>
 </head>
 <body>
@@ -58,7 +58,7 @@
 <script src="{{ asset('public/vendors/vue/vue.js') }}"></script>
 <script src="{{ asset('public/vendors/vue/vue-router.js') }}"></script>
 <script src="{{ asset('public/vendors/vue/axios.min.js') }}"></script>
-<script src="{{ asset('public/vendors/weui/zepto.min.js') }}"></script>
+
 <script src="{{ asset('public/vendors/weui/weui.min.js') }}"></script>
 
 <script>
