d95e50691b304773f2e85a1bd7f9e48c147ab302
songzidaiyu <1151879477@qq.com>
2017-11-30 15:53:17 +0800

    1

--- public/vue/fabu.js
+++ public/vue/fabu.js
@@ -4,7 +4,18 @@
 fabu = Vue.component('fabu', function (success, error) {
     axios.get("/public/tpl/fatie.html").then(function (res) {
         success({
-            template: res.data
+            template: res.data,
+            data: {
+                title:'',
+                content:''
+            },
+            methods:{
+                send: function(){
+                    console.log(this.title);
+                    console.log(this.content);
+
+                }
+            }
         });
     });
 });
