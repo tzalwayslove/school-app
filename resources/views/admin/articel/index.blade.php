422e8419a162d844b1c3defdd7c370495d914c56
songzidaiyu <1151879477@qq.com>
2017-12-01 10:25:01 +0800

    1

--- resources/views/admin/articel/index.blade.php
+++ resources/views/admin/articel/index.blade.php
@@ -47,7 +47,7 @@
                                                 <td>{{ $v->content }}</td>
                                                 <td>{{ $v->zan or 0 }}</td>
                                                 <td>{{ isset($v->user_account)? $v->user_account->account : '匿名' }}</td>
-                                                <td>{{ $v->cate_name->name or '无' }}</td>
+                                                <td>{{ isset($v->cate_name)? $v->cate_name->name : '无' }}</td>
                                                 @php$arr = [];$arr = ['1'=>'0',]@endphp
                                                 <td>{{ $arr[$v->show] }}</td>
 
