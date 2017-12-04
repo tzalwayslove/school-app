32dcd5d452b8c93e94d1baa3da6d68afff8cd1c1
songzidaiyu <1151879477@qq.com>
2017-12-04 14:18:06 +0800

    成绩

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::post('code', 'Admin\CodeController@gettable');
 Route::put('code', 'Admin\CodeController@settable');
 
 Route::get('test', function(){
-    dd(\App\Model\User::whereOpinId('ocDq7wTnH5dh9n09aNxRV0jrc05c')->first());
+    dd(\App\Model\User::whereOpenId('ocDq7wTnH5dh9n09aNxRV0jrc05c')->first());
 });
 
 Route::group([
