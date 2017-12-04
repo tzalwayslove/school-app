b52d62a44c464e63f414628ba179c30f4d7c2dcf
songzidaiyu <1151879477@qq.com>
2017-12-04 16:09:17 +0800

    成绩

--- app/Model/Dom/Kechengbiao.php
+++ app/Model/Dom/Kechengbiao.php
@@ -9,15 +9,16 @@ use Carbon\Carbon;
 class Kechengbiao extends Login
 {
     public $weekNum;
+    private $diffDay = 0;
     public function __construct($user_name, $password)
     {
         parent::__construct($user_name, $password);
 
         $start_school = new Carbon(Setting::getOne('start_school'));
-        $now = new Carbon();
+        $now = new Carbon(time());
 
-        $diffDays = $now->diffInDays($start_school);
-
-        $this->weekNum = intval($diffDays / 7);
+        $this->diffDays = $now->diffInDays($start_school);
+        dd($this->diffDays);
+        $this->weekNum = intval($this->diffDays / 7);
     }
 }
