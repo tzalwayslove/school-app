ad0ead390463df75d816a2d0beffe55ccfe0b02a
songzidaiyu <1151879477@qq.com>
2017-12-05 13:22:25 +0800

    课程表

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::post('code', 'Admin\CodeController@gettable');
 Route::put('code', 'Admin\CodeController@settable');
 
 Route::get('test', function(){
-     $kaochang = new \App\Model\Dom\Kaochang('', '');
+     $kaochang = new \App\Model\Dom\Kaochang('201637025002', 'liuxuemin123');
      $kaochang->getQueryData();
 });
 
