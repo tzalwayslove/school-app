5ab1082a37964c7dd9615a0d2f94a336fe8dcdc3
songzidaiyu <1151879477@qq.com>
2017-12-05 15:17:55 +0800

    文章

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,16 +10,16 @@
     <title>Gentelella Alela! | </title>
 
     <!-- Bootstrap -->
-    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('public/vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="{{asset('public/vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
     <!-- NProgress -->
-    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <link href="{{ asset('public/vendors/nprogress/nprogress.css) }}" rel="stylesheet">
     <!-- Animate.css -->
-    <link href="../vendors/animate.css/animate.min.css" rel="stylesheet">
+    <link href="{{ asset('public/vendors/animate.css/animate.min.css) }}" rel="stylesheet">
 
     <!-- Custom Theme Style -->
-    <link href="../build/css/custom.min.css" rel="stylesheet">
+    <link href="{{ asset('public/build/css/custom.min.css) }}" rel="stylesheet">
 </head>
 
 <body class="login">
