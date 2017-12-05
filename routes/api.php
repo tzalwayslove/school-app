47c395ce58dc3c99729bc8eed740cf90a102e7f8
songzidaiyu <1151879477@qq.com>
2017-12-05 14:02:26 +0800

    课程表

--- routes/api.php
+++ routes/api.php
@@ -60,4 +60,4 @@ Route::get('chengji', 'Wx\UserController@nowChengji');
 Route::get('chengji_all', 'Wx\UserController@all');
 
 Route::get('kecheng', 'Wx\UserController@kecheng');
-Route::get('kaochang', 'Wx\userController@kaochang');
+Route::get('kaochang', 'Wx\UserController@kaochang');
