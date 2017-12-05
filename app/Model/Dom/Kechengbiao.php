2385c458a1831d0bd3652335d9edfbac5acb1b57
songzidaiyu <1151879477@qq.com>
2017-12-05 11:05:41 +0800

    课程表

--- app/Model/Dom/Kechengbiao.php
+++ app/Model/Dom/Kechengbiao.php
@@ -23,8 +23,6 @@ class Kechengbiao extends Login
         $diffDays = $now->diffInDays($start_school);
         $this->weekNum = intval($diffDays / 7);
 
-        dd($this->weekNum);
-
     }
 
     /**
