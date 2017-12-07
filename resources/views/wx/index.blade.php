a3d04949797428e19218fc599aedc04b7cd4ff07
songzidaiyu <1151879477@qq.com>
2017-12-07 15:48:07 +0800

    1

--- resources/views/wx/index.blade.php
+++ resources/views/wx/index.blade.php
@@ -13,6 +13,7 @@
         .weui-cells {
             margin-top: 0
         }
+
         #add_pinglun {
             position: fixed;
             padding: 20px;
@@ -23,13 +24,16 @@
             z-index: 100;
             background: #eee;
         }
-        #add_pinglun input{
-            width:80%;
+
+        #add_pinglun input {
+            width: 80%;
         }
-        #add_pinglun button{
+
+        #add_pinglun button {
             width: 15%;
         }
-        #add_pinglun_show{
+
+        #add_pinglun_show {
             position: fixed;
             right: 20px;
             bottom: 70px;
@@ -43,7 +47,8 @@
             font-size: 66px;
             color: #FFF;
         }
-        #remen{
+
+        #remen {
             position: fixed;
             right: 20px;
             bottom: 70px;
@@ -63,28 +68,31 @@
     <div class="page tabfabu js_show">
         <div class="page__bd" style="height: 100%;">
             <div class="weui-tab">
-                    <router-view></router-view>
+                <router-view></router-view>
                 <div class="weui-tabbar">
                     <router-link to="/tiezi"
                                  v-bind:class="['weui-tabbar__item', nav_active == 'tiezi' ? 'weui-bar__item_on' : '']">
                         <div style="height: 10px;"></div>
-                        <span style="display: inline-block;position: relative;">
+                    <span style="display: inline-block;position: relative;">
                         <img src="{{asset('public/images/icon-index.png')}}" alt="" class="weui-tabbar__icon">
                     </span>
                         <div style="height: 10px;"></div>
                     </router-link>
                     <router-link to="/fabu"
                                  v-bind:class="['weui-tabbar__item', nav_active == 'fabu' ? 'weui-bar__item_on' : '']">
+                        <div style="height: 10px;"></div>
+                    <span style="display: inline-block;position: relative;">
                         <img src="{{asset('public/images/icon-fabu.png')}}" alt="" class="weui-tabbar__icon">
+                    </span>
                         {{--<p class="weui-tabbar__label">发帖</p>--}}
                         <div style="height: 10px;"></div>
                     </router-link>
                     <router-link to="/wode"
                                  v-bind:class="['weui-tabbar__item', nav_active == 'wode' ? 'weui-bar__item_on' : '']">
-                        <div style="height: 10px;"></div>
+
                         <span style="display: inline-block;position: relative;">
                         <img src="{{asset('public/images/icon-wode.png')}}" alt="" class="weui-tabbar__icon">
-                    </span>
+                        </span>
                         <div style="height: 10px;"></div>
                     </router-link>
                 </div>
@@ -112,7 +120,7 @@
         {path: '/tiezi', component: tiezi, name: 'tiezi'},
         {path: '/fabu', component: fabu, name: 'fabu'},
         {path: '/wode', component: wode, name: 'wode'},
-        {path: '/pinglun/:id', component: pinglun, name:'pinglun', props:{articel: 0}}
+        {path: '/pinglun/:id', component: pinglun, name: 'pinglun', props: {articel: 0}}
     ];
 
     const router = new VueRouter({
@@ -122,12 +130,12 @@
 
     data = {
         nav_active: 'tiezi',
-        tiezi:[],
-        user:{}
+        tiezi: [],
+        user: {}
     };
 
     router.beforeEach((to, from, next) => {
-        switch(to.name){
+        switch (to.name) {
             case "pinglun":
                 data.nav_active = 'tiezi';
                 break;
@@ -142,10 +150,8 @@
         router: router,
         data: data,
         load: true,
-        methods: {
-
-        },
-        mounted:function(){
+        methods: {},
+        mounted: function () {
             router.push({
                 path: '/tiezi'
             })
