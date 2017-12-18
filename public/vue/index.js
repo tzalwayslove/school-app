cb5db5256cb8f2f19e85d0c7266f820558c9183b
songzidaiyu <1151879477@qq.com>
2017-12-18 10:00:58 +0800

    1

--- public/vue/index.js
+++ public/vue/index.js
@@ -75,7 +75,7 @@ tiezi = Vue.component('tiezi', function (success, error) {
                 onScroll: function (e) {
 
                     let bottom = $('#tabbar1')[0].scrollHeight - $('#tabbar1')[0].scrollTop - $('#tabbar1')[0].offsetHeight;
-                    console.log($('#tabbar1')[0].scrollHeight);
+                    console.log($('#tabbar1')[0].scrollTop);
                     console.log('bottom');
                     if (this.lastBottom > bottom && bottom < $('#tabbar1')[0].clientHeight / 3 && this.load) {
                         this.page++;
