ba28d271bf4895b9787519a22e6f56e37e259a9d
songzidaiyu <1151879477@qq.com>
2017-12-18 10:06:08 +0800

    1

--- resources/views/wx/index.blade.php
+++ resources/views/wx/index.blade.php
@@ -1,9 +1,10 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>遇见</title>
     {{--<script src="{{ asset('public/wx/js/mui.min.js') }}"></script>--}}
