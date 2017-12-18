321b41548e8c8ed2a455b7fa83faaa83d3f70c91
songzidaiyu <1151879477@qq.com>
2017-12-18 15:19:56 +0800

    1

--- public/vue/wode.js
+++ public/vue/wode.js
@@ -6,12 +6,13 @@ wode = Vue.component('wode', function (resolve, reject) {
     axios.get("/public/tpl/wode.html").then(function (res) {
         resolve({
             data:function(){
-
+                return {
+                    user:null
+                }
             },
             template: res.data,
             mounted:function(){
-                console.log(33333333);
-                console.log(vue.user);
+                this.user = vue.user;
             }
         });
     });
