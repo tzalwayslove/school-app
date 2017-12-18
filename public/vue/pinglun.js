5d181bf2bdd2553095f1448b2637f198576393d6
songzidaiyu <1151879477@qq.com>
2017-12-18 16:43:32 +0800

    1

--- public/vue/pinglun.js
+++ public/vue/pinglun.js
@@ -63,7 +63,7 @@ pinglun = Vue.component('pinglun', function (success, error) {
 
                     axios.get('wx/comment/'+ id).then(function(res){
                         $this.articelData = res.data.data;
-                        console.log($this.articelData);
+                        console.log($this.articelData.avatar);
                         $this.list = res.data.data.get_comment;
                         if($this.list.length == 0){
 
