217fa36ebc8c14f8bbf7228b7ef9fef3e1b0e011
songzidaiyu <1151879477@qq.com>
2017-12-18 13:12:56 +0800

    1

--- public/vue/index.js
+++ public/vue/index.js
@@ -94,8 +94,10 @@ tiezi = Vue.component('tiezi', function (success, error) {
                     item.zanLog = !!!item.zanLog;
                 },
                 orderConvert(){
-                    this.mui_active = this.mui_active == 'remen'? 'zuixin' : 'remen';
+                    this.mui_active = this.mui_active == 'remen' ? 'zuixin' : 'remen';
+                    console.log(this.mui_active);
                     this.getData(0);
+
                 },
                 getElementTop: function (element) {
                     actualTop = element.offsetTop;
