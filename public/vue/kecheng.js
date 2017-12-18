b9d3b7b174cb15a1bc6d8dfe1d2c6bb3961a0f64
songzidaiyu <1151879477@qq.com>
2017-12-18 15:26:50 +0800

    1

--- public/vue/kecheng.js
+++ public/vue/kecheng.js
@@ -18,7 +18,10 @@ fabu = Vue.component('Kecheng', function (success, error) {
             },
             mounted: function(){
                 $this = this;
+                console.log(111111);
                 console.log(this.user);
+                console.log(111111);
+
                 axios.get("/api/kecheng?user="+this.user + '&all='+this.all).then(function(res){
                     $this.jiazai = false;
                     if(res.data.result.code == 0){
