3003279c1b44ad64c4b65e84aeab9f99bdf230f5
songzidaiyu <1151879477@qq.com>
2017-12-19 10:10:37 +0800

    1

--- app/Model/Dom/Login.php
+++ app/Model/Dom/Login.php
@@ -46,7 +46,7 @@ class Login
         $this->client = new Client();
 
         $url = $this->pre . '/xsd/xk/LoginToXk';
-        $res = $this->client->request('get', 'http://www.baidu.com');
+        $res = $this->client->request('get', '1900mx9281.51mypc.cn');
         dd($res);
 
         $res = $this->client->request('post', $url, [
