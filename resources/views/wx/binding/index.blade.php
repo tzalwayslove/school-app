3d5f31929bbd81ac026c761df57c2b753182de08
songzidaiyu <1151879477@qq.com>
2017-12-19 09:13:42 +0800

    1

--- resources/views/wx/binding/index.blade.php
+++ resources/views/wx/binding/index.blade.php
@@ -24,7 +24,7 @@
     密码:<input type="text" style="background-color:#3cc4b6;border:none;margin: 0;" placeholder="请输入教务处密码" id="password"/>
 </div>
 <div style="margin:30px 15% 10px;">
-    <input type="button" value="确认绑定" style="width: 100%; text-align: center;border: 1px solid #FFFFFF;border-radius: 20px;margin: 0;background: #FFFFFF;color: #3CC3B6;" />
+    <input type="button" value="确认绑定" style="width: 100%; text-align: center;border: 1px solid #FFFFFF;border-radius: 20px;margin: 0;background: #FFFFFF;color: #3CC3B6;" id="binding"/>
     <p style="font-size: 10px;color: #FFFFFF;text-align: center;">您尚未绑定，请先进行绑定开启你的教务旅程</p>
 </div>
 <div class="fix-bott">
@@ -35,22 +35,24 @@
 <script src="{{ asset('/public/js/jquery-laravel-ajax.js') }}"></script>
 <script>
     $(function(){
-        account = $('#account').val();
-        password = $('#password').val();
-        if(!account == '' || password == ''){
-            alert('请输入正确账户和密码!');
-        }
-        data = {
-            account:account,
-            password:password,
-        };
-        postData('/wx/binding', data, function(res){
-            if(res.result.code == 1){
-                //跳转
-                window.location.href='/wx/bind-success';
-            }else{
-                alert('绑定失败，请稍后再试!'||res.result.message);
+        $('#binding').on('click', function(){
+            account = $('#account').val();
+            password = $('#password').val();
+            if(!account == '' || password == ''){
+                alert('请输入正确账户和密码!');
             }
+            data = {
+                account:account,
+                password:password,
+            };
+            postData('/wx/binding', data, function(res){
+                if(res.result.code == 1){
+                    //跳转
+                    window.location.href='/wx/bind-success';
+                }else{
+                    alert('绑定失败，请稍后再试!'||res.result.message);
+                }
+            });
         });
     });
 </script>
