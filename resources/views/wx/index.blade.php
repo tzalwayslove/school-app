3e207d9aed10a76311040224691c0fb40512da8e
songzidaiyu <1151879477@qq.com>
2017-12-19 15:10:53 +0800

    1

--- resources/views/wx/index.blade.php
+++ resources/views/wx/index.blade.php
@@ -19,7 +19,7 @@
 <body>
 
 <div id="app">
-    <nav class="mui-bar mui-bar-tab" style="background-color: #ffffff;">
+    <nav class="mui-bar mui-bar-tab" style="background-color: #ffffff;" v-show="bar-buttom">
          {{--route link block--}}
         <router-link class="mui-tab-item mui-active mui-active" to="/tiezi">
             <img src="{{ asset('public/wx/img/icon-index.png')}}" class="item-logo">
@@ -33,101 +33,7 @@
     </nav>
     <div class="mui-content">
         {{-- route main --}}
-
         <router-view></router-view>
-
-        {{--<div id="tabbar2" class="mui-control-content">
-            <div id="" class="box2">
-                <p class="fb-title">发布 <span style="float: right;margin-right: 10px;">匿名</span>
-                <div style="float: right; margin-top: -30PX;margin-right: 50px;">
-
-                    <div class="mui-switch mui-active">
-                        <div class="mui-switch-handle"></div>
-                    </div>
-                </div>
-
-                </p>
-                <textarea rows="5" maxlength="150" placeholder="写下你想说的话"></textarea>
-                <p class="fb-sub">
-                    <input type="submit" value="取消"/>
-                    <input type="submit" value="发布"/>
-                </p>
-
-            </div>
-
-        </div>
-        <div id="tabbar3" class="mui-control-content ">
-            <div class="box2-t">
-                <a><img src="{{asset('public/wx/img/m-touxiang.png')}}" class="box2-t-logo"></a><br/>
-                <a style="margin-bottom: 10px;">某同学·男</a><br/>
-                <div class="md6" style="border-right: 1px solid #FFFFFF;"><a>帖子 100</a></div>
-                <div class="md6"><a>帖子 100</a></div>
-                <a href="classtable.html">
-                    <div class="md4">
-                        <img src="{{asset('public/wx/img/icon-kebiao.png')}}" class="box2-t-logo2"/>
-                        <p>课表</p>
-                    </div>
-                </a>
-                <a href="transcript.html">
-                    <div class="md4">
-                        <img src="{{asset('public/wx/img/icon-cj.png')}}" class="box2-t-logo2"/>
-                        <p>成绩</p>
-                    </div>
-                </a>
-                <a href="test-table.html">
-                    <div class="md4">
-                        <img src="{{asset('public/wx/img/icon-kc.png')}}" class="box2-t-logo2"/>
-                        <p>考场</p>
-                    </div>
-                </a>
-                <div class="md4">
-                    <img src="{{asset('public/wx/img/icon-xl.png')}}" class="box2-t-logo2"/>
-                    <p>校历</p>
-                </div>
-                <div class="md4">
-                    <img src="{{asset('public/wx/img/icon-ts.png')}}" class="box2-t-logo2"/>
-                    <p>图书馆</p>
-                </div>
-                <div class="md4">
-                    <img src="{{asset('public/wx/img/icon-kjs.png')}}" class="box2-t-logo2"/>
-                    <p>空教室</p>
-                </div>
-            </div>
-            <div class="box2-m">
-                <ul class="mui-table-view">
-                    <li class="mui-table-view-cell">
-                        <a class="mui-navigate-right">
-                            校内电话簿
-                        </a>
-                    </li>
-                    <li class="mui-table-view-cell">
-                        <a class="mui-navigate-right">
-                            教务处网络
-                        </a>
-                    </li>
-                    <li class="mui-table-view-cell">
-                        <a class="mui-navigate-right">
-                            四六级成绩
-                        </a>
-                    </li>
-                    <li class="mui-table-view-cell">
-                        <a class="mui-navigate-right">
-                            座位预约
-                        </a>
-                    </li>
-                    <li class="mui-table-view-cell">
-                        <a class="mui-navigate-right">
-                            忘记密码
-                        </a>
-                    </li>
-                    <li class="mui-table-view-cell">
-                        <a class="mui-navigate-right">
-                            联系我们
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>--}}
     </div>
 </div>
 
@@ -157,7 +63,8 @@
         data = {
             nav_active: 'tiezi',
             tiezi: [],
-            user: '{{ $user->id }}'
+            user: '{{ $user->id }}',
+            barButtom: true
         };
 
         router.beforeEach((to, from, next) => {
