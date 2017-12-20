6412aa7b1fe9b82e76feb767ed09783f61e2857c
songzidaiyu <1151879477@qq.com>
2017-12-20 11:30:47 +0800

    1

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -59,7 +59,7 @@
                 <!-- sidebar menu -->
                 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
                     <div class="menu_section">
-                        <h3>General</h3>
+                        <h3></h3>
                         <ul class="nav side-menu">
                             <li><a><i class="fa fa-edit"></i> 学生列表 <span class="fa fa-chevron-down"></span></a>
                                 <ul class="nav child_menu">
@@ -69,7 +69,7 @@
                         </ul>
                     </div>
                     <div class="menu_section">
-                        <h3>General</h3>
+                        <h3></h3>
                         <ul class="nav side-menu">
                             <li><a><i class="fa fa-edit"></i> 文章 <span class="fa fa-chevron-down"></span></a>
                                 <ul class="nav child_menu">
