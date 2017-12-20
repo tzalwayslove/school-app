977563c27572e9caebf683ead14a52839ce33d10
songzidaiyu <1151879477@qq.com>
2017-12-20 09:27:04 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -1,59 +1,59 @@
 <?php
 use EasyWeChat\Foundation\Application;
-/*
-Route::get('/', function () {
-    $user = session('user');
-    return view('wx.index', compact('user'));
-})->middleware('wx_login');
-
-Route::get('/s', function(){
-    session(['a'=>10]);
-});
-
-Route::get('/g', function(){
-    dd(session('a'));
-});
-
-
-Route::post('upload', 'UploadController@index');
-Route::delete('upload', 'UploadController@removeUpload');
-
-Route::get('code', 'Admin\CodeController@index');
-Route::post('code', 'Admin\CodeController@gettable');
-Route::put('code', 'Admin\CodeController@settable');
-
-Route::get('test', function(){
-     /*$kaochang = new \App\Model\Dom\Kaochang('201637025002', 'liuxuemin123');
-     $kaochang->getQueryData();*/
-     /*$chengji = new \App\Model\Dom\Chengji('201723045019', 'smyqueen980818');
-     $chengji->getChengji();*/
-
-     /*$pingjiao = new \App\Model\Dom\Pingjiao('201723045019', 'smyqueen980818');
-     $pingjiao->pingjiao();*/
-
-     /*$chengji = new \App\Model\Dom\Chengji('201637025002', 'liuxuemin123');
-     $res = $chengji->getChengji();
-     dd($res);*/
-//     $renxing = new \App\Model\Dom\RenxingLogin('rxwudanqian', 'wudanqian');
-//    $renxing->dingdan();
-    return view('wx.binding.index');
-});
-
-Route::group([
-    'prefix'=>'admin',
-    'namespace'=> 'Admin',
-    'middleware'=>[
-        'auth'
-    ]
-], function(){
-    Route::get('/', 'AdminController@index');
-    Route::resource('user', 'UserController');
-    Route::resource('cate', 'CateController');
-    Route::resource('articel', 'ArticelController');
-    Route::resource('comment', 'CommentController');
-});
-
-Route::any('wx/login', 'Wx\Login@index');*/
+//
+//Route::get('/', function () {
+//    $user = session('user');
+//    return view('wx.index', compact('user'));
+//})->middleware('wx_login');
+//
+//Route::get('/s', function(){
+//    session(['a'=>10]);
+//});
+//
+//Route::get('/g', function(){
+//    dd(session('a'));
+//});
+//
+//
+//Route::post('upload', 'UploadController@index');
+//Route::delete('upload', 'UploadController@removeUpload');
+//
+//Route::get('code', 'Admin\CodeController@index');
+//Route::post('code', 'Admin\CodeController@gettable');
+//Route::put('code', 'Admin\CodeController@settable');
+//
+//Route::get('test', function(){
+//     /*$kaochang = new \App\Model\Dom\Kaochang('201637025002', 'liuxuemin123');
+//     $kaochang->getQueryData();*/
+//     /*$chengji = new \App\Model\Dom\Chengji('201723045019', 'smyqueen980818');
+//     $chengji->getChengji();*/
+//
+//     /*$pingjiao = new \App\Model\Dom\Pingjiao('201723045019', 'smyqueen980818');
+//     $pingjiao->pingjiao();*/
+//
+//     /*$chengji = new \App\Model\Dom\Chengji('201637025002', 'liuxuemin123');
+//     $res = $chengji->getChengji();
+//     dd($res);*/
+////     $renxing = new \App\Model\Dom\RenxingLogin('rxwudanqian', 'wudanqian');
+////    $renxing->dingdan();
+//    return view('wx.binding.index');
+//});
+//
+//Route::group([
+//    'prefix'=>'admin',
+//    'namespace'=> 'Admin',
+//    'middleware'=>[
+//        'auth'
+//    ]
+//], function(){
+//    Route::get('/', 'AdminController@index');
+//    Route::resource('user', 'UserController');
+//    Route::resource('cate', 'CateController');
+//    Route::resource('articel', 'ArticelController');
+//    Route::resource('comment', 'CommentController');
+//});
+//
+//Route::any('wx/login', 'Wx\Login@index');
 
 /*Route::group([
     'prefix'=>'wx',
@@ -93,7 +93,8 @@ Route::any('wx/login', 'Wx\Login@index');*/
     Route::get('/bind-success', function(){
         return view('wx.binding.success');
     });
-});*//*
+});*/
+/*
 Route::get('wx_menu', function(){
     $option = require 'wechatConfig.php';
 
@@ -136,26 +137,6 @@ Route::get('wx_menu', function(){
             "key" => "考场"
         ]
     ];
-        /*[
-            "name"       => "菜单",
-            "sub_button" => [
-                [
-                    "type" => "view",
-                    "name" => "搜索",
-                    "url"  => "http://www.soso.com/"
-                ],
-                [
-                    "type" => "view",
-                    "name" => "视频",
-                    "url"  => "http://v.qq.com/"
-                ],
-                [
-                    "type" => "click",
-                    "name" => "赞一下我们",
-                    "key" => "V1001_GOOD"
-                ],
-            ],
-        ],*/
     $menu->add($buttons);
 });
 Auth::routes();
