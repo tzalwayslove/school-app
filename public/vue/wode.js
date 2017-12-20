5bd53cac3f38ba0cdc4b28410e27ea63aa583a57
songzidaiyu <1151879477@qq.com>
2017-12-20 13:07:39 +0800

    1

--- public/vue/wode.js
+++ public/vue/wode.js
@@ -16,13 +16,15 @@ wode = Vue.component('wode', function (resolve, reject) {
                         avatar:'/public/wx/img/m-touxiang.png',
                         zan:'...',
                         tiezi:'...'
-                    }
+                    },
+                    bindurl:''
                 }
             },
             template: res.data,
             mounted:function(){
                 this.userId = data.user;
                 $this = this;
+                this.bindurl = '/wx/binding/?user='+this.userId;
                 axios.get('/api/get/user?user='+this.userId).then(function(res){
                     console.log(res);
                     $this.user = res.data;
