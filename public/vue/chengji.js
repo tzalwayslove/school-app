40e45fbac15ee4bb5db3d68d09d105598f16fbf5
1151879477@qq.com <1151879477@qq.com>
2017-12-21 21:21:44 +0800

    1

--- public/vue/chengji.js
+++ public/vue/chengji.js
@@ -47,7 +47,7 @@ fabu = Vue.component('chengji', function (success, error) {
                     }else{
                         $this.chengji = res.data.chengji;
                         $this.jige =$this.chengji.filter(function(item){
-                            return item.chengji >= 60;
+                            return item.chengji >= 60 || item.chengji.indexOf('不及格') == -1;
                         });
                         $this.bujige = $this.chengji.filter(function(item){
                             return item.chengji < 60;
