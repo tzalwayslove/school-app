afdd59169899606f8a0f3a98692bb7d827ca1b9e
1151879477@qq.com <1151879477@qq.com>
2017-12-21 22:50:45 +0800

    1

--- resources/views/wx/yikatong/login.blade.php
+++ resources/views/wx/yikatong/login.blade.php
@@ -10,15 +10,33 @@
 </head>
 <body>
 
-<form action="{{ url('/wx/yikatonglogin') }}" method="post">
+<form action="{{ url('/wx/yikatonglogin') }}" method="post" id="form">
     {{ csrf_field() }}
     <img src="{{ url('/wx/yikatongCode') }}" alt="">
-    <input type="text" name="user_name" placeholder="用户名">
-    <input type="text" name="password" placeholder="密码">
-    <input type="number" name="code" placeholder="验证码" maxlength="2">
+    <input type="text" name="user_name" placeholder="用户名" id="user_name">
+    <input type="text" name="password" placeholder="密码" id="password">
+    <input type="number" name="code" placeholder="验证码" maxlength="2" id="code">
     <button id="login">登录</button>
 </form>
 <script src="{{ asset('/public/vendors/jquery/dist/jquery.js') }}"></script>
 <script src="{{ asset('/public/js/jquery-laravel-ajax.js') }}"></script>
+<script>
+    $(function(){
+        $('#login').on('click', function(){
+            data = {
+                user_name : $('#user_name').val(),
+                password : $('#password').val(),
+                code:$('#code').val()
+            };
+            $.postData($('#form').attr('action'), data, function(res){
+                if(res.result.code == 1){
+                    alert('登录成功');
+                }else{
+                    alert('登录失败');
+                }
+            })
+        });
+    });
+</script>
 </body>
 </html>
