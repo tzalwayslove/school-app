318fc1d048187ab2e123cdbeb86beff480d304cf
1151879477@qq.com <1151879477@qq.com>
2017-12-21 22:47:33 +0800

    1

--- resources/views/wx/yikatong/login.blade.php
+++ resources/views/wx/yikatong/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>一卡通登录</title>
 </head>
 <body>
@@ -15,8 +16,9 @@
     <input type="text" name="user_name" placeholder="用户名">
     <input type="text" name="password" placeholder="密码">
     <input type="number" name="code" placeholder="验证码" maxlength="2">
-    <input type="submit">
+    <button id="login">登录</button>
 </form>
-
+<script src="{{ asset('/public/vendors/jquery/dist/jquery.js') }}"></script>
+<script src="{{ asset('/public/js/jquery-laravel-ajax.js') }}"></script>
 </body>
 </html>
