2a041ca8912d1da246cba11a293739902ef9d5fd
1151879477@qq.com <1151879477@qq.com>
2017-12-21 22:43:01 +0800

    1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\WxRedirect;
+use App\Http\Middleware\Yikatong;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -57,6 +58,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'wx_login'=>WxRedirect::class
+        'wx_login'=>WxRedirect::class,
+        'yikatong'=>Yikatong::class,
     ];
 }
