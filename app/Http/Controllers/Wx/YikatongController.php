0524557cae6725b109490f8efacb1ea64f328599
songzidaiyu <1151879477@qq.com>
2017-12-22 12:00:23 +0800

    1

--- app/Http/Controllers/Wx/YikatongController.php
+++ app/Http/Controllers/Wx/YikatongController.php
@@ -77,7 +77,11 @@ class YikatongController extends Controller
         $liushui = new \App\Model\Dom\Liushui($user->account, '123456');
 
         try{
-            $data = $liushui->getData($start_time, $end_time, $page);
+            $res = $liushui->getData($start_time, $end_time, $page);
+            $data = [];
+            foreach($res as $item){
+                $data[] = $item;
+            }
             return [
                 'result'=>new Result(true),
                 'liushui'=>$data
