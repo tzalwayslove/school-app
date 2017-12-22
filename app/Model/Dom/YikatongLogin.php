a04dd1915a767e271e1ef9229a3277a73f01ec88
songzidaiyu <1151879477@qq.com>
2017-12-22 10:28:20 +0800

    1

--- app/Model/Dom/YikatongLogin.php
+++ app/Model/Dom/YikatongLogin.php
@@ -27,8 +27,8 @@ class YikatongLogin
         $this->user_name = $user_name;
         $this->passwrod = $password;
 
-        if(session('isLogin') && $jar = session('cookie_jar')){
-            $jar = unserialize($jar);
+        if(session('isLogin') && session('cookie_jar')){
+            $jar = session('cookie_jar');
         }else{
             $jar = new CookieJar();
         }
