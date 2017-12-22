61c35c9882aae0165eb21027270071c1e8e2f066
songzidaiyu <1151879477@qq.com>
2017-12-22 15:49:01 +0800

    1

--- app/Model/Dom/Login.php
+++ app/Model/Dom/Login.php
@@ -91,7 +91,7 @@ class Login
             $info['id_card'] = $idCard->text();
             $info['yikatong_password'] = substr($this->id_card, -6);
         }
-
+        dd($info);
         User::where('account', $this->account)->update($info);
 
         return true;
