8dacb2413a47cfce5e1119caa20539c5f9d3b65f
songzidaiyu <1151879477@qq.com>
2017-12-22 08:35:51 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -107,12 +107,12 @@ Route::group([
     Route::get('yikatongLogin', 'YikatongController@login');
     Route::post('yikatongLogin', 'YikatongController@doLogin');
 
-    /*Route::group([
+    Route::group([
         'middleware'=>['yikatong'],
         'prefix'=>'yikatong',
     ], function(){
-
-    });*/
+        Route::get('liushui', 'YikatongController@liushui');
+    });
 
 
 });
