51462d4da202ad8f6ad3381832b44160a49d7c1c
songzidaiyu <1151879477@qq.com>
2017-12-22 15:19:46 +0800

    1

--- public/vue/liushui.js
+++ public/vue/liushui.js
@@ -29,10 +29,10 @@ fabu = Vue.component('liushui', function (success, error) {
                     console.log(this.selected);
                     this.getData()
                 },
+
                 getData:function(cover = 1){
                     $this =this;
                     this.jiazai = true;
-                    console.log(this.page);
 
                     start_time = "19991130";
                     end_time = "20171222";
