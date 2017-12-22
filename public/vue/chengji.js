55ad319e2b50c0a0891bafc90f9b2c2a2b1e7081
songzidaiyu <1151879477@qq.com>
2017-12-22 12:37:44 +0800

    1

--- public/vue/chengji.js
+++ public/vue/chengji.js
@@ -20,7 +20,7 @@ fabu = Vue.component('chengji', function (success, error) {
                     xuefenjidian = 0;
                     xuefen = 0;
                     for ( i = 0, len = this.chengji.length; i < len; ++i ){
-                        console.log(this.chengji[i]);
+                        console.log(this.chengji[i].xuefen);
                         // xuefenjidian += this.chengji[i].xuefen * this.chengji[i].jidian;
                         // xufen += this.chengji[i].xuefen;
                     }
