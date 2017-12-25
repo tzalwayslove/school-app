672f4e4fc7b3368b56e7bdb9109cbceb6d57b446
songzidaiyu <1151879477@qq.com>
2017-12-25 13:08:07 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -79,7 +79,7 @@ Route::group([
     Route::post('/comment_zan', 'CommentController@zan');
     Route::post('/addComment', 'CommentController@addComment');
     Route::post('/reply', 'CommentController@reply');
-    Route::post('/jubao', 'JubaoController@jubao');
+    Route::post('/jubao', 'CommentController@jubao');
 
     Route::get('/chengji', function (\Illuminate\Http\Request $request) {
         return view('wx.chengji.index')->withUser($request->input('user'));
