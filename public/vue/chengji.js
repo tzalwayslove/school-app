030dbe01f28f3a1d721b84337481bbeb5d774cbf
songzidaiyu <1151879477@qq.com>
2017-12-25 09:08:30 +0800

    1

--- public/vue/chengji.js
+++ public/vue/chengji.js
@@ -6,69 +6,75 @@ fabu = Vue.component('chengji', function (success, error) {
         success({
             template: res.data,
             props: ['user'],
-            data(){
+            data() {
                 return {
-                    jiazai:true,
-                    chengji:[],
-                    jige:[],
-                    bujige:[],
-                    page: 'newest',
-                    options:[]
+                    jiazai: true,
+                    chengji: [],
+                    jige: [],
+                    bujige: [],
+                    selected: '',
+                    options: [],
+
                 }
             },
             computed: {
-                GPA:function(){
+                GPA: function () {
                     xuefenjidian = 0;
                     xuefen = 0;
 
-                    for ( i = 0, len = this.chengji.length; i < len; ++i ){
+                    for (i = 0, len = this.chengji.length; i < len; ++i) {
                         this.chengji[i].xuefen = Number(this.chengji[i].xuefen);
                         this.chengji[i].jidian = Number(this.chengji[i].xuefen);
                         xuefenjidian += this.chengji[i].xuefen * this.chengji[i].jidian;
                         xuefen += this.chengji[i].xuefen;
                     }
 
-                    console.log(xuefenjidian);
-                    console.log(xuefen);
 
                     return xuefen > 0
                         ? (xuefenjidian / xuefen).toFixed(2)
                         : 0;
                 }
             },
-            methods:{
-                changePage: function(){
-                    if(this.page == 'all'){
-                        window.location.href='/wx/chengji_all?user='+this.user
+            methods: {
+                changePage: function () {
+                    if (this.selected == 'all') {
+                        window.location.href = '/wx/chengji_all?user=' + this.user
+                    } else {
+                        this.jiazai = true;
+                        this.getData();
                     }
+                },
+                getData: function () {
+                    $this = this;
+                    axios.get("/api/chengji?user=" + this.user + '&xueqi='+this.selected).then(function (res) {
+                        $this.jiazai = false;
+                        if (res.data.result.code == 0) {
+                            alert(res.data.result.message || '获取失败!');
+                        } else {
+                            $this.chengji = res.data.chengji;
+                            $this.jige = $this.chengji.filter(function (item) {
+                                return item.chengji >= 60 || item.chengji.indexOf('不及格') == -1;
+                            });
+                            $this.bujige = $this.chengji.filter(function (item) {
+                                return item.chengji < 60;
+                            });
+                        }
+                    });
                 }
             },
 
-            mounted: function(){
+            mounted: function () {
                 $this = this;
 
-                oxios.get("/wx/getChengjiOptions?user="+this.user).then(function(res){
+                oxios.get("/wx/getChengjiOptions?user=" + this.user).then(function (res) {
                     res = res.data;
-                    if(res.result.code == 1){
+                    if (res.result.code == 1) {
                         $this.options = res.options;
-                    }else{
+                    } else {
                         alert('请求数据失败，请重试！');
                     }
                 });
-                axios.get("/api/chengji?user="+this.user).then(function(res){
-                    $this.jiazai = false;
-                    if(res.data.result.code == 0){
-                        alert(res.data.result.message || '获取失败!');
-                    }else{
-                        $this.chengji = res.data.chengji;
-                        $this.jige =$this.chengji.filter(function(item){
-                            return item.chengji >= 60 || item.chengji.indexOf('不及格') == -1;
-                        });
-                        $this.bujige = $this.chengji.filter(function(item){
-                            return item.chengji < 60;
-                        });
-                    }
-                });
+                this.getData();
 
             }
         });
