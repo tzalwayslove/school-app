bb008960717ae9ec4a2e41697e36c80ae0429a11
songzidaiyu <1151879477@qq.com>
2017-12-26 15:33:25 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,7 @@ Route::group([
         $year = substr($user->account, 0, 4);
         $str = $year. '-09-01';
         $ruxue = strtotime($str);
-        $day = ceil(time() -$ruxue/ 60/ 60 /24);
+        $day = ceil(time() -$ruxue/ (60* 60 *24));
         $user->day = $day;
         dd($day);
         return view('wx.kecheng.index')->withRequest($request)->withUser($user);
