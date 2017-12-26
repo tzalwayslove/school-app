47a680d176822aa518ae80413a921567f658bf1a
songzidaiyu <1151879477@qq.com>
2017-12-26 15:18:34 +0800

    1

--- app/Model/Dom/Login.php
+++ app/Model/Dom/Login.php
@@ -80,11 +80,12 @@ class Login
             return false;
         }
 
-
         $name = $table->filterXPath('//tr[4]/td[2]');
         $info = [];
         if($name->count()){
             $info['name'] = trim($name->text());
+
+            dd($info);
         }
 //        48
         $idCard = $table->filterXPath('//tr[48]/td[4]');
