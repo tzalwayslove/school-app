d70c779de8673991e792a30fdd903e801a297dbe
songzidaiyu <1151879477@qq.com>
2017-12-26 15:23:03 +0800

    1

--- app/Model/Dom/Login.php
+++ app/Model/Dom/Login.php
@@ -83,8 +83,7 @@ class Login
         $name = $table->filterXPath('//tr[4]/td[2]');
         $info = [];
         if($name->count()){
-            $info['name'] = trim($name->text(), ' ');
-
+            $info['name'] = substr($name->text(), 1);
         }
 //        48
         $idCard = $table->filterXPath('//tr[48]/td[4]');
