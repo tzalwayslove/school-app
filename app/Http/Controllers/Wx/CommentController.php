0b673c6640aae8871649716c124c11df268b4a28
songzidaiyu <1151879477@qq.com>
2017-12-26 18:18:12 +0800

    1

--- app/Http/Controllers/Wx/CommentController.php
+++ app/Http/Controllers/Wx/CommentController.php
@@ -26,7 +26,6 @@ class CommentController extends Controller
         $data->_user_account = User::find($data->user);
         foreach($data->getComment as $comment){
             $comment->_get_reply = Comment::where('id', $comment->reply)->first();
-            dd($comment);
             $comment->_get_reply->_get_user = User::find($comment->_get_reply->user);
             $comment->_get_user = User::find($comment->user);
         }
