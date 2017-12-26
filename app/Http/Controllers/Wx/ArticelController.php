ec09230d1b047216360c21b6de6def608934346c
songzidaiyu <1151879477@qq.com>
2017-12-26 17:48:33 +0800

    1

--- app/Http/Controllers/Wx/ArticelController.php
+++ app/Http/Controllers/Wx/ArticelController.php
@@ -33,7 +33,7 @@ class ArticelController extends Controller
         foreach($list as $item){
             $item->commentCount = count($item->getComment);
             $item->_created_at = Articel::getTimeAgo($item->created_at->__toString());
-            $item->user_account = User::find($item->user);
+            $item->user_account = 1;//User::find($item->user);
         }
 
         return response(['result'=>new Result(true), 'list'=> $list]);
