b17225532696dbfbd9e917a2d1e722b4aa9364ac
songzidaiyu <1151879477@qq.com>
2017-12-27 17:30:08 +0800

    1

--- app/Http/Middleware/Yikatong.php
+++ app/Http/Middleware/Yikatong.php
@@ -16,7 +16,7 @@ class Yikatong
     public function handle($request, Closure $next)
     {
         if(!session('isLogin')){
-            session(['target'=>$request->fullUrl()]);
+            session(['target'=>$request->fullUrl() ? $request->fullUrl() : url('/yikatong/liushui?user='.$request->input('user'))]);
             return redirect('wx/yikatong/login?user='.$request->input('user'));
         }
         return $next($request);
