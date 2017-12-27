fd65bd92cbe4d90bc0e21040750d1d6387c616d3
songzidaiyu <1151879477@qq.com>
2017-12-27 13:44:09 +0800

    1

--- app/Console/Commands/score_push.php
+++ app/Console/Commands/score_push.php
@@ -63,7 +63,7 @@ class score_push extends Command
         }
         $t2 = microtime(true);
         echo '耗时'.round($t2-$t1,3).'秒<br>';
-        echo 'Now memory_get_usage: ' . memory_get_usage() . "\n";
+        echo 'Now memory_get_usage: ' . memory_get_usage() / 1000 . "Kb\n";
         return 1;
     }
 }
