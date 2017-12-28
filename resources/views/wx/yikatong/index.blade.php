4f540ee861f953eb20f204c4132ff1d3c0915628
liyang <liyang@qq.com>
2017-12-28 16:23:01 +0800

    1

--- resources/views/wx/yikatong/index.blade.php
+++ resources/views/wx/yikatong/index.blade.php
@@ -21,7 +21,7 @@
             padding: 10px;
             margin: 20px 20px 0 20px;
             border: 1px solid #eee;
-            border-left-width: 5px;
+            border-width: 5px;
             border-radius: 3px;
             border-color:#3cc4b6;
             font-size: 16px;
